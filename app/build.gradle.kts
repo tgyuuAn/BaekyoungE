390879a490780085edf05b14341d1e8d6f1b1868
tgyuu-An <atg0614@gmail.com>
2023-11-21 17:38:00 +0900

    faeture #2 : 백경, 뿌공 로그인 화면 에 넣기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
     implementation(libs.bundles.androidx)
     implementation(libs.bundles.compose)
-    implementation(libs.bundles.compose.navigation)
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/tgyuu/baekyoung_i/MainActivity.kt
+++ app/src/main/java/com/tgyuu/baekyoung_i/MainActivity.kt
@@ -3,8 +3,16 @@ package com.tgyuu.baekyoung_i
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.tgyuu.baekyoung_i.ui.theme.BaekyoungiTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BaekyoungiTheme {
+                AuthScreen()
             }
         }
     }
@@ -19,7 +28,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AuthScreen() {
-    Box() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_auth_bbugong),
+            contentDescription = stringResource(id = R.string.bbugong_auth_conext_descriptioin),
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+        )
 
+        Image(
+            painter = painterResource(id = R.drawable.ic_auth_baekyoung),
+            contentDescription = stringResource(id = R.string.bbugong_auth_conext_descriptioin),
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+        )
     }
 }
