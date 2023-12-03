7bbb8dd3039aa575acccf8392069faad343ade5f
tgyuu-An <atg0614@gmail.com>
2023-12-03 21:07:11 +0900

    add #1 : bottomnavigation selected vector

--- app/src/main/java/com/tgyuu/baekyoung_i/main/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/tgyuu/baekyoung_i/main/navigation/TopLevelDestination.kt
@@ -2,6 +2,10 @@ package com.tgyuu.baekyoung_i.main.navigation
 
 import android.graphics.drawable.Icon
 import androidx.annotation.StringRes
+import androidx.compose.material.Icon
+import androidx.compose.ui.res.painterResource
+import com.tgyuu.baekyoung_i.R
+import com.tgyuu.baekyoung_i.home.navigation.homeNavigationRoute
 
 enum class TopLevelDestination(
     val route: String,
@@ -10,4 +14,11 @@ enum class TopLevelDestination(
     @StringRes val iconTextId: Int,
     @StringRes val titleTextId: Int,
 ) {
+    HOME(
+        route = homeNavigationRoute,
+        selectedIcon = Icon(
+            painterResource(id = R.drawable.ic_selected_home),
+            contentDescription = null,
+        ),
+        unselectedIcon = Icon()
 }
