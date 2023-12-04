fc1eb217e5e6c53e41fd697e617687b922e730b9
tgyuu-An <atg0614@gmail.com>
2023-12-04 18:43:26 +0900

    add #7 : hackers

--- app/src/main/java/com/tgyuu/baekyoung_i/home/HomeScreen.kt
+++ app/src/main/java/com/tgyuu/baekyoung_i/home/HomeScreen.kt
@@ -1,16 +1,21 @@
 package com.tgyuu.baekyoung_i.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -38,6 +43,21 @@ fun HomeScreen() {
                 modifier = Modifier
                     .padding(start = 20.dp)
             )
+        }
+
+        Column() {
+            Row(modifier = Modifier.weight(1f)) {
+                Card(
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Image(painter = painterResource(id = ), contentDescription = null)
+                }
+            }
+
+            Row(modifier = Modifier.weight(1f)) {
+
+            }
         }
     }
 }
