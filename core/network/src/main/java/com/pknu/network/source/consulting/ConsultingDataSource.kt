8a4866b86dc439233c8eb9f42447283fcfaffb00
tgyuu-An <atg0614@gmail.com>
2023-12-21 12:26:51 +0900

    feature #9 : ConsultingDataSourceImpl

--- core/network/src/main/java/com/pknu/network/source/consulting/ConsultingDataSource.kt
+++ core/network/src/main/java/com/pknu/network/source/consulting/ConsultingDataSource.kt
@@ -1,11 +1,13 @@
 package com.pknu.network.source.consulting
 
+import com.pknu.network.model.consulting.ConsultingRequest
+import com.pknu.network.model.consulting.ConsultingResponse
 import kotlinx.coroutines.flow.Flow
 
 interface ConsultingDataSource {
-    suspend fun postConsultingInformation(): Result<Unit>
+    suspend fun postConsultingInformation(consultingRequest: ConsultingRequest): Result<Unit>
 
-    suspend fun postUserChatting(): Result<Unit>
+    suspend fun postUserChatting(consultingResponse: ConsultingResponse): Result<Unit>
 
-    fun getAssistantChatting(): Flow<Result<Unit>>
+    suspend fun getAssistantChatting(): Flow<Result<ConsultingResponse>>
 }
--- core/network/src/main/java/com/pknu/network/source/consulting/ConsultingDataSourceImpl.kt
+++ core/network/src/main/java/com/pknu/network/source/consulting/ConsultingDataSourceImpl.kt
@@ -1,21 +1,49 @@
 package com.pknu.network.source.consulting
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
+import com.pknu.network.constant.CHATTING_COLLECTION
+import com.pknu.network.constant.USER_INFORMATION_COLLECTION
+import com.pknu.network.model.consulting.ConsultingRequest
+import com.pknu.network.model.consulting.ConsultingResponse
+import com.pknu.network.util.await
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ConsultingDataSourceImpl @Inject constructor(
     private val firebaseFirestore: FirebaseFirestore,
 ) : ConsultingDataSource {
-    override suspend fun postConsultingInformation(): Result<Unit> = runCatching {
-        firebaseFirestore.collection()
-    }
 
-    override suspend fun postUserChatting(): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun postConsultingInformation(consultingRequest: ConsultingRequest): Result<Unit> =
+        runCatching {
+            val task = firebaseFirestore.collection(USER_INFORMATION_COLLECTION)
+                .document("8I9y4HW94HGLiTlxPCFf")
+                .set(consultingRequest)
+                .await()
+        }
 
-    override fun getAssistantChatting(): Flow<Result<Unit>> {
-        TODO("Not yet implemented")
+    override suspend fun postUserChatting(consultingResponse: ConsultingResponse): Result<Unit> =
+        runCatching {
+            val task = firebaseFirestore.collection(CHATTING_COLLECTION)
+                .document("K2PrpK1UBiJU6ZXUsQh1")
+                .set(consultingResponse)
+                .await()
+        }
+
+    override suspend fun getAssistantChatting(): Flow<Result<ConsultingResponse>> = flow {
+        while (true) {
+            val consulting = runCatching {
+                val task = firebaseFirestore.collection(CHATTING_COLLECTION)
+                    .document("K2PrpK1UBiJU6ZXUsQh1")
+                    .get()
+                    .await()
+
+                checkNotNull(task.toObject<ConsultingResponse>())
+            }
+            emit(consulting)
+            delay(500L)
+        }
     }
 }
