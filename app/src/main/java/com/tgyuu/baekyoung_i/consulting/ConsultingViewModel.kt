7104c740eb94315c925a4afecb185b9f1666ea21
tgyuu-An <atg0614@gmail.com>
2023-12-21 13:54:11 +0900

    add #9 : grade, major StateFlow

--- app/src/main/java/com/tgyuu/baekyoung_i/consulting/ConsultingViewModel.kt
+++ app/src/main/java/com/tgyuu/baekyoung_i/consulting/ConsultingViewModel.kt
@@ -5,6 +5,8 @@ import com.pknu.domain.usecase.consulting.GetConsultingChattingUseCase
 import com.pknu.domain.usecase.consulting.PostConsultingInformationUseCase
 import com.pknu.domain.usecase.consulting.PostUserChattingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,4 +16,9 @@ class ConsultingViewModel @Inject constructor(
     private val getConsultingChattingUseCase: GetConsultingChattingUseCase,
 ) : ViewModel() {
 
+    private val _grade: MutableStateFlow<Int> = MutableStateFlow(0)
+    val grade get() = _grade.asStateFlow()
+
+    private val _major: MutableStateFlow<String> = MutableStateFlow("")
+    val major get() = _major.asStateFlow()
 }
