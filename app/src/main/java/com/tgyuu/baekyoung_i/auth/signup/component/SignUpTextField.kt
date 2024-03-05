903152ae219190083be5bd7923aa4cc854b413df
tgyuu-An <atg0614@gmail.com>
2024-03-06 02:30:11 +0900

    feature #18 : SignUpTextField

--- app/src/main/java/com/tgyuu/baekyoung_i/auth/signup/component/SignUpTextField.kt
+++ app/src/main/java/com/tgyuu/baekyoung_i/auth/signup/component/SignUpTextField.kt
@@ -2,22 +2,34 @@ package com.tgyuu.baekyoung_i.auth.signup.component
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.tgyuu.designsystem.theme.BaekyoungTheme
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 internal fun SignUpTextField(
     @StringRes title: Int,
     @StringRes hint: Int,
     value: String,
-    onValueChanged: (String) -> Unit,
+    onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(
         verticalArrangement = Arrangement.spacedBy(4.dp),
         modifier = modifier
@@ -26,6 +38,33 @@ internal fun SignUpTextField(
             text = stringResource(id = title),
             style = BaekyoungTheme.typography.contentBold,
             color = BaekyoungTheme.colors.black56,
+            modifier = Modifier.padding(start = 5.dp),
         )
+
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = BaekyoungTheme.typography.labelRegular.copy(
+                color = BaekyoungTheme.colors.black56
+            ),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
+        ){ innerTextField ->
+            Box(modifier = Modifier.padding(vertical = 10.dp, horizontal = 12.dp)) {
+                if (value.isEmpty()) {
+                    Text(
+                        text = stringResource(id = hint),
+                        color = BaekyoungTheme.colors.grayAC,
+                        style = BaekyoungTheme.typography.labelRegular,
+                        modifier = Modifier.align(Alignment.CenterStart),
+                    )
+                }
+
+                Box(modifier = Modifier.align(Alignment.CenterStart)) {
+                    innerTextField()
+                }
+            }
+        }
     }
 }
