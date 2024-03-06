5dda953d34f0acbd9ab1a4e1c2a1ad800716fe41
tgyuu-An <atg0614@gmail.com>
2024-03-06 11:57:40 +0900

    feature #18 : BaekyoungButton

--- app/src/main/java/com/tgyuu/baekyoung_i/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/tgyuu/baekyoung_i/auth/signup/SignUpScreen.kt
@@ -1,12 +1,21 @@
 package com.tgyuu.baekyoung_i.auth.signup
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.tgyuu.baekyoung_i.R
 import com.tgyuu.baekyoung_i.auth.signup.component.SignUpTextField
@@ -21,19 +30,63 @@ internal fun SignUpRoute(navigateToHome: () -> Unit) {
 
 @Composable
 internal fun SignUpScreen(navigateToHome: () -> Unit) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(BaekyoungTheme.colors.blueF5FF)
     ) {
-        SignUpTextField(
-            title = R.string.nickname,
-            hint = R.string.nickname_hint,
-            value = "",
-            onValueChange = {},
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(start = 24.dp, end = 24.dp, top = 60.dp),
-        )
+        ) {
+            SignUpTextField(
+                title = R.string.nickname,
+                hint = R.string.nickname_hint,
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Text(
+                text = stringResource(id = R.string.sex),
+                style = BaekyoungTheme.typography.contentBold,
+                color = BaekyoungTheme.colors.black56,
+                modifier = Modifier.padding(start = 5.dp),
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = BaekyoungTheme.colors.white,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = BaekyoungTheme.colors.grayD0,
+                        shape = RoundedCornerShape(10.dp),
+                    )
+            ) {
+                Text(
+                    text = "남성",
+                    style = BaekyoungTheme.typography.labelRegular,
+                    color = BaekyoungTheme.colors.black56,
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(horizontal = 12.dp, vertical = 6.dp),
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_arrow_down),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(horizontal = 12.dp, vertical = 6.dp)
+                )
+
+            }
+        }
     }
 }
---
+++ core/designsystem/src/main/java/com/tgyuu/designsystem/component/BaekyoungButton.kt
@@ -0,0 +1,39 @@
+package com.tgyuu.designsystem.component
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.tgyuu.designsystem.theme.BaekyoungTheme
+
+@Composable
+fun BaekyoungButton(
+    @StringRes text: Int,
+    buttonColor: Color = BaekyoungTheme.colors.blueDD,
+    textColor: Color = BaekyoungTheme.colors.white,
+    onButtonClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(10.dp))
+            .background(buttonColor)
+            .clickable { onButtonClick() },
+    ) {
+        Text(
+            text = stringResource(id = text),
+            style = BaekyoungTheme.typography.contentBold,
+            color = textColor,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
