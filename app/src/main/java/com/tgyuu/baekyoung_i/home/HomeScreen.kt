7fc8508304ecb3a713690e360ba4571b3e1cd60b
tgyuu-An <atg0614@gmail.com>
2024-03-19 16:39:44 +0900

    add #20 : 채팅, 세팅 벡터 추가

--- app/src/main/java/com/tgyuu/baekyoung_i/home/HomeScreen.kt
+++ app/src/main/java/com/tgyuu/baekyoung_i/home/HomeScreen.kt
@@ -2,17 +2,19 @@ package com.tgyuu.baekyoung_i.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalFocusManager
@@ -65,8 +67,26 @@ internal fun HomeScreen() {
                 .padding(start = 16.dp, end = 16.dp, top = 16.dp)
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(10.dp))
-                .background(BaekyoungTheme.colors.white.copy(alpha = 0.3f)),
+                .background(BaekyoungTheme.colors.white.copy(alpha = 0.4f)),
         ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .padding(10.dp),
+            ) {
+                Image(
+                    painterResource(id = R.drawable.ic_fish),
+                    contentDescription = null,
+                )
+
+                Text(
+                    text = "50",
+                    style = BaekyoungTheme.typography.contentBold,
+                    color = BaekyoungTheme.colors.white,
+                )
+            }
         }
     }
 }
