a9aaa56ef644fb2f904ffe6ead2ff285d01b33e3
tgyuu-An <atg0614@gmail.com>
2024-03-25 13:51:49 +0900

    feature #28 : signUp -> Home Navigation만 애니메이션 소거 후 나머지 Navigation은 애니메이션 적용

--- app/src/main/java/com/tgyuu/baekyounge/main/navigation/BaekyoungNavHost.kt
+++ app/src/main/java/com/tgyuu/baekyounge/main/navigation/BaekyoungNavHost.kt
@@ -2,6 +2,9 @@ package com.tgyuu.baekyounge.main.navigation
 
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -18,6 +21,7 @@ import com.tgyuu.baekyounge.consulting.chatting.navigation.navigateToChatting
 import com.tgyuu.baekyounge.consulting.consultinginformation.navigation.consultingGraph
 import com.tgyuu.baekyounge.consulting.consultinginformation.navigation.consultingNavigationRoute
 import com.tgyuu.baekyounge.etc.navigation.etcScreen
+import com.tgyuu.baekyounge.home.navigation.homeNavigationRoute
 import com.tgyuu.baekyounge.home.navigation.homeScreen
 import com.tgyuu.baekyounge.home.navigation.navigateToHome
 import com.tgyuu.baekyounge.shop.navigation.shopScreen
@@ -33,8 +37,22 @@ fun BaekyoungNavHost(
 ) {
     NavHost(
         navController = navController,
-        enterTransition = { EnterTransition.None },
-        exitTransition = { ExitTransition.None },
+        enterTransition = {
+            if (navController.currentDestination?.route == homeNavigationRoute &&
+                navController.previousBackStackEntry?.destination?.route == signUpNavigationRoute
+            ) {
+                EnterTransition.None
+            } else {
+                fadeIn(animationSpec = tween(700))
+            }
+        },
+        exitTransition = {
+            if (navController.currentDestination?.route == signUpNavigationRoute) {
+                ExitTransition.None
+            } else {
+                fadeOut(animationSpec = tween(700))
+            }
+        },
         startDestination = startDestination,
         modifier = modifier,
     ) {
