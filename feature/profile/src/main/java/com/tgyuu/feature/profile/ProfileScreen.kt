fd739e3e7f6a3461a0827cada3a01248dde55329
tgyuu-An <atg0614@gmail.com>
2024-04-14 14:30:19 +0900

    feature #37 : 프로필 초기 화면 임시 저장

--- feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileScreen.kt
@@ -2,11 +2,14 @@ package com.tgyuu.feature.profile
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,9 +19,11 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tgyuu.designsystem.R
 import com.tgyuu.designsystem.theme.BaekyoungTheme
 
 @Composable
@@ -45,11 +50,58 @@ fun ProfileScreen() {
             )
 
             Image(
-                painter = painterResource(id = com.tgyuu.designsystem.R.drawable.ic_arrow_right),
+                painter = painterResource(id = R.drawable.ic_arrow_right),
                 contentDescription = null,
                 modifier = Modifier.align(Alignment.CenterEnd),
             )
         }
+
+        HorizontalDivider(color = BaekyoungTheme.colors.grayD0)
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(BaekyoungTheme.colors.whiteFF),
+        ) {
+            Text(
+                text = "함께한 지 18 일",
+                style = BaekyoungTheme.typography.labelRegular,
+                modifier = Modifier.padding(top = 10.dp, start = 20.dp),
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp, bottom = 5.dp),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.align(Alignment.Center),
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_consulting_note),
+                        contentDescription = null,
+                    )
+
+                    Text(
+                        text = "2",
+                        style = BaekyoungTheme.typography.contentBold,
+                        modifier = Modifier.padding(start = 5.dp),
+                    )
+                }
+            }
+
+            Text(
+                text = "완료한 상담",
+                textAlign = TextAlign.Center,
+                style = BaekyoungTheme.typography.labelRegular,
+                modifier = Modifier
+                    .padding(bottom = 30.dp)
+                    .fillMaxWidth(),
+            )
+        }
+
+        HorizontalDivider(color = BaekyoungTheme.colors.grayD0)
     }
 }
 
