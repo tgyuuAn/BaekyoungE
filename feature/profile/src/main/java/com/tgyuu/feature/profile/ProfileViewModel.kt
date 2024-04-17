6c02f5c167465cf6cd25a45b818b2f58cde90ead
tgyuu-An <atg0614@gmail.com>
2024-04-17 14:58:45 +0900

    feature #37 : Consulting 화면 실제 데이터로 변경!

--- feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileViewModel.kt
@@ -25,7 +25,6 @@ class ProfileViewModel @Inject constructor(
     }
 
     fun checkTokenExists() = viewModelScope.launch {
-        // 토큰이 없을 경우 바로 Auth 페이지로 이동
         if (!AuthApiClient.instance.hasToken()) {
             _userInformation.value = UiState.Error("유저 정보가 없습니다.")
             return@launch
@@ -36,12 +35,12 @@ class ProfileViewModel @Inject constructor(
                 _userInformation.value = UiState.Error("유저 정보가 없습니다.")
             }
 
-            verifyMemberId(tokenInfo?.id ?: -1)
+            getUserInformation(tokenInfo?.id ?: -1)
             return@accessTokenInfo
         }
     }
 
-    fun verifyMemberId(userId: Long) = viewModelScope.launch {
+    fun getUserInformation(userId: Long) = viewModelScope.launch {
         getUserInformationUseCase(userId.toString())
             .onSuccess {
                 _userInformation.value = UiState.Success(it)
