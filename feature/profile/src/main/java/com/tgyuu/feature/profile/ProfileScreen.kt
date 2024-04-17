95ab118d8d0caf858f1db86d2a7a77daaecd5c99
tgyuu-An <atg0614@gmail.com>
2024-04-17 14:13:05 +0900

    feature #37 : Profile Screen에서 실제 User정보를 받아오도록 변경

--- feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,116 +26,138 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.tgyuu.common.util.UiState
 import com.tgyuu.designsystem.R
+import com.tgyuu.designsystem.component.Loader
 import com.tgyuu.designsystem.theme.BaekyoungTheme
+import com.tgyuu.model.auth.UserInformation
 
 @Composable
 internal fun ProfileRoute(
     navigateToSetting: () -> Unit,
     viewModel: ProfileViewModel = hiltViewModel(),
 ) {
-    ProfileScreen(navigateToSetting = navigateToSetting)
+    val userInformationState by viewModel.userInformation.collectAsStateWithLifecycle()
+
+    ProfileScreen(
+        userInformationState = userInformationState,
+        navigateToSetting = navigateToSetting,
+    )
 }
 
 @Composable
-fun ProfileScreen(navigateToSetting: () -> Unit) {
+fun ProfileScreen(
+    userInformationState: UiState<UserInformation>,
+    navigateToSetting: () -> Unit,
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(BaekyoungTheme.colors.white),
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-        ) {
-            Text(
-                text = generateUserNameSpan(userName = "종디기"),
-                style = BaekyoungTheme.typography.labelBold,
-                modifier = Modifier.align(Alignment.CenterStart),
-            )
+        when (userInformationState) {
+            is UiState.Loading -> Loader(modifier = Modifier.fillMaxSize())
+            is UiState.Success -> {
+                val userInformation = userInformationState.data
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                ) {
+                    Text(
+                        text = generateUserNameSpan(userName = userInformation.nickName),
+                        style = BaekyoungTheme.typography.labelBold,
+                        modifier = Modifier.align(Alignment.CenterStart),
+                    )
 
-            Image(
-                painter = painterResource(id = R.drawable.ic_arrow_right),
-                contentDescription = null,
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .clickable { navigateToSetting() },
-            )
-        }
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_arrow_right),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .align(Alignment.CenterEnd)
+                            .clickable { navigateToSetting() },
+                    )
+                }
 
-        HorizontalDivider(color = BaekyoungTheme.colors.grayDC)
+                HorizontalDivider(color = BaekyoungTheme.colors.grayDC)
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(BaekyoungTheme.colors.grayF0),
-        ) {
-            Text(
-                text = generateDaysSinceRegistrationSpan(days = 18),
-                style = BaekyoungTheme.typography.labelRegular,
-                modifier = Modifier.padding(top = 10.dp, start = 20.dp),
-            )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(BaekyoungTheme.colors.grayF0),
+                ) {
+                    Text(
+                        text = generateDaysSinceRegistrationSpan(days = 18),
+                        style = BaekyoungTheme.typography.labelRegular,
+                        modifier = Modifier.padding(top = 10.dp, start = 20.dp),
+                    )
 
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp, bottom = 5.dp),
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.align(Alignment.Center),
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 20.dp, bottom = 5.dp),
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.align(Alignment.Center),
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.ic_consulting_note),
+                                contentDescription = null,
+                            )
+
+                            Text(
+                                text = "2",
+                                style = BaekyoungTheme.typography.contentBold,
+                                modifier = Modifier.padding(start = 5.dp),
+                            )
+                        }
+                    }
+
+                    Text(
+                        text = "완료한 상담",
+                        textAlign = TextAlign.Center,
+                        style = BaekyoungTheme.typography.labelRegular,
+                        modifier = Modifier
+                            .padding(bottom = 30.dp)
+                            .fillMaxWidth(),
+                    )
+                }
+
+                HorizontalDivider(color = BaekyoungTheme.colors.grayDC)
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 40.dp),
                 ) {
                     Image(
-                        painter = painterResource(R.drawable.ic_consulting_note),
+                        painter = painterResource(id = com.tgyuu.feature.profile.R.drawable.ic_suggestion_box),
                         contentDescription = null,
                     )
 
                     Text(
-                        text = "2",
-                        style = BaekyoungTheme.typography.contentBold,
-                        modifier = Modifier.padding(start = 5.dp),
+                        "건의함",
+                        style = BaekyoungTheme.typography.labelRegular.copy(fontSize = 10.sp),
+                        color = BaekyoungTheme.colors.gray95,
+                        modifier = Modifier.padding(top = 4.dp),
                     )
                 }
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                HorizontalDivider(
+                    color = BaekyoungTheme.colors.grayDC,
+                    modifier = Modifier.padding(bottom = 60.dp),
+                )
             }
-
-            Text(
-                text = "완료한 상담",
-                textAlign = TextAlign.Center,
-                style = BaekyoungTheme.typography.labelRegular,
-                modifier = Modifier
-                    .padding(bottom = 30.dp)
-                    .fillMaxWidth(),
-            )
+            is UiState.Error -> {
+                // Todo
+            }
+            is UiState.Init -> {}
         }
-
-        HorizontalDivider(color = BaekyoungTheme.colors.grayDC)
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 40.dp),
-        ) {
-            Image(
-                painter = painterResource(id = com.tgyuu.feature.profile.R.drawable.ic_suggestion_box),
-                contentDescription = null,
-            )
-
-            Text(
-                "건의함",
-                style = BaekyoungTheme.typography.labelRegular.copy(fontSize = 10.sp),
-                color = BaekyoungTheme.colors.gray95,
-                modifier = Modifier.padding(top = 4.dp),
-            )
-        }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        HorizontalDivider(
-            color = BaekyoungTheme.colors.grayDC,
-            modifier = Modifier.padding(bottom = 60.dp),
-        )
     }
 }
 
@@ -153,16 +176,17 @@ private fun generateUserNameSpan(userName: String): AnnotatedString = buildAnnot
 }
 
 @Composable
-private fun generateDaysSinceRegistrationSpan(days: Int): AnnotatedString = buildAnnotatedString {
-    append("함께한 지 ")
-    withStyle(
-        style = SpanStyle(
-            color = BaekyoungTheme.colors.black,
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp,
-        ),
-    ) {
-        append(days.toString())
+private fun generateDaysSinceRegistrationSpan(days: Int): AnnotatedString =
+    buildAnnotatedString {
+        append("함께한 지 ")
+        withStyle(
+            style = SpanStyle(
+                color = BaekyoungTheme.colors.black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+            ),
+        ) {
+            append(days.toString())
+        }
+        append(" 일")
     }
-    append(" 일")
-}
--- feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/tgyuu/feature/profile/ProfileViewModel.kt
@@ -4,12 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.user.UserApiClient
+import com.tgyuu.common.util.UiState
 import com.tgyuu.domain.usecase.auth.GetUserInformationUseCase
 import com.tgyuu.model.auth.UserInformation
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,26 +17,23 @@ import javax.inject.Inject
 class ProfileViewModel @Inject constructor(
     private val getUserInformationUseCase: GetUserInformationUseCase,
 ) : ViewModel() {
-    private val _eventFlow = MutableSharedFlow<ProfileEvent>()
-    val eventFlow = _eventFlow.asSharedFlow()
-
-    private val _userInformation = MutableStateFlow(UserInformation())
+    private val _userInformation = MutableStateFlow<UiState<UserInformation>>(UiState.Loading)
     val userInformation = _userInformation.asStateFlow()
 
     init {
         checkTokenExists()
     }
 
-    fun checkTokenExists() {
+    fun checkTokenExists() = viewModelScope.launch {
         // 토큰이 없을 경우 바로 Auth 페이지로 이동
         if (!AuthApiClient.instance.hasToken()) {
-            event(ProfileEvent.UserLoadingFailed)
-            return
+            _userInformation.value = UiState.Error("유저 정보가 없습니다.")
+            return@launch
         }
 
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
             if (error != null) {
-                event(ProfileEvent.UserLoadingFailed)
+                _userInformation.value = UiState.Error("유저 정보가 없습니다.")
             }
 
             verifyMemberId(tokenInfo?.id ?: -1)
@@ -48,16 +44,8 @@ class ProfileViewModel @Inject constructor(
     fun verifyMemberId(userId: Long) = viewModelScope.launch {
         getUserInformationUseCase(userId.toString())
             .onSuccess {
-                _userInformation.value = it
-                event(ProfileEvent.UserLoadingSuccess)
+                _userInformation.value = UiState.Success(it)
             }
-            .onFailure { event(ProfileEvent.UserLoadingFailed) }
-    }
-
-    fun event(event: ProfileEvent) = viewModelScope.launch { _eventFlow.emit(event) }
-
-    sealed class ProfileEvent {
-        data object UserLoadingFailed : ProfileEvent()
-        data object UserLoadingSuccess : ProfileEvent()
+            .onFailure { _userInformation.value = UiState.Error("유저 정보가 없습니다.") }
     }
 }
