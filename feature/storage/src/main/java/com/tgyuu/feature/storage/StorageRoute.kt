887a183848a2bd4c0eaa9e254022f027a200c443
tgyuu-An <atg0614@gmail.com>
2024-04-29 15:21:24 +0900

    feature #41 : Spinner에서 ModalBottomSheet로 바로 변경 키키

--- feature/storage/src/main/java/com/tgyuu/feature/storage/StorageRoute.kt
+++ feature/storage/src/main/java/com/tgyuu/feature/storage/StorageRoute.kt
@@ -5,21 +5,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,12 +31,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.sd.lib.compose.wheel_picker.FVerticalWheelPicker
+import com.sd.lib.compose.wheel_picker.FWheelPickerState
+import com.sd.lib.compose.wheel_picker.rememberFWheelPickerState
 import com.tgyuu.designsystem.R.drawable
+import com.tgyuu.designsystem.component.BaekyoungModalBottomSheet
 import com.tgyuu.designsystem.component.BaekyoungTopBar
 import com.tgyuu.designsystem.theme.BaekyoungTheme
 
@@ -43,29 +49,92 @@ import com.tgyuu.designsystem.theme.BaekyoungTheme
 internal fun StorageRoute(viewModel: StorageViewModel = hiltViewModel()) {
     val yearList by viewModel.yearList.collectAsStateWithLifecycle()
     val selectedYear by viewModel.selectedYear.collectAsStateWithLifecycle()
+    val yearPickerState = rememberFWheelPickerState()
 
     StorageScreen(
         yearList = yearList,
         selectedYear = selectedYear,
+        yearPickerState = yearPickerState,
         setSelectedYear = viewModel::setSelectedYear,
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun StorageScreen(
     yearList: List<String>,
     selectedYear: String,
+    yearPickerState: FWheelPickerState,
     setSelectedYear: (String) -> Unit,
 ) {
+    val sheetState = rememberModalBottomSheetState()
     var showBottomSheet by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = { BaekyoungTopBar(titleTextId = R.string.storage) },
         containerColor = BaekyoungTheme.colors.grayF5,
-        modifier = Modifier
-            .fillMaxSize()
-            .clickable { showBottomSheet = false },
+        modifier = Modifier.fillMaxSize()
     ) { paddingValues ->
+        if (showBottomSheet) {
+            BaekyoungModalBottomSheet(
+                sheetState = sheetState,
+                onDissmissRequest = { showBottomSheet = false },
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Text(
+                        text = "기록을 보고싶은 년도를 선택하세요.",
+                        style = BaekyoungTheme.typography.labelBold.copy(
+                            fontSize = 14.sp,
+                        ),
+                        color = BaekyoungTheme.colors.black,
+                        modifier = Modifier.padding(top = 20.dp),
+                    )
+
+                    FVerticalWheelPicker(
+                        modifier = Modifier.width(50.dp),
+                        count = 1,
+                        state = yearPickerState,
+                    ) { year ->
+                        Text(
+                            text = "${year + 2024} 년",
+                            style = BaekyoungTheme.typography.labelBold.copy(
+                                fontSize = 14.sp,
+                            ),
+                            color = BaekyoungTheme.colors.black,
+                        )
+                    }
+
+                    Button(
+                        onClick = { showBottomSheet = false },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = BaekyoungTheme.colors.gray95,
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                        modifier = Modifier.padding(
+                            bottom = 20.dp,
+                            start = 20.dp,
+                            end = 20.dp,
+                        ),
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.complete),
+                            style = BaekyoungTheme.typography.labelBold,
+                            color = BaekyoungTheme.colors.white,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.CenterVertically)
+                                .padding(vertical = 8.dp),
+                        )
+                    }
+                }
+            }
+        }
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -139,10 +208,6 @@ internal fun StorageScreen(
                     }
                 }
             }
-        }
-
-        if (showBottomSheet) {
-
         }
     }
 }
