887f1839e42ee3fe9c0de8c5df110227b70038a2
tgyuu-An <atg0614@gmail.com>
2024-06-02 15:10:29 +0900

    feature #47 : analytics에 로그 저장

--- core/network/src/main/java/com/tgyuu/network/di/FirebaseModule.kt
+++ core/network/src/main/java/com/tgyuu/network/di/FirebaseModule.kt
@@ -1,5 +1,7 @@
 package com.tgyuu.network.di
 
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -16,4 +18,6 @@ object FirebaseModule {
     @Provides
     @Singleton
     fun providesFirebaseStore(): FirebaseFirestore = Firebase.firestore
+
+    val firebaseAnalytics = Firebase.analytics
 }
